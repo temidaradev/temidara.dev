b3fcd9eda05a9b16ada5637ddd8727ababa749d0
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-08-26 21:59:22 +0300

    draft

--- src/views/Experiences.tsx
+++ src/views/Experiences.tsx
@@ -2,74 +2,82 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import tw from 'twin.macro';
 
-import ProjectItem from '../components/ProjectItem';
-import ProjectSkeleton from '../components/ProjectSkeleton';
-import useHandling from '../hooks/use-handling';
-import ProjectModel from '../models/ProjectModel';
-import github, { Direction, RepositorySort, RepositoryType } from '../services/github';
-
 const Wrapper = tw.main`mx-auto w-full max-w-screen-lg px-8 py-12`;
 
 const Title = tw.h2`text-2xl text-slate-700`;
 
 const List = tw.div`mt-8 grid grid-cols-1 lg:grid-cols-2 gap-2`;
 
-export default function Projects() {
-  const { t } = useTranslation();
-  const [projects, setProjects] = useState<ProjectModel[]>([]);
+const ExpComp = tw.div`flex gap-3 p-3 border border-white rounded-sm`;
 
-  const [loadingProjects, loadProject] = useHandling(
-    useCallback(async () => {
-      const list = await github.listRepositories({
-        type: RepositoryType.All,
-        sort: RepositorySort.Pushed,
-        direction: Direction.Desc,
-        page: 1,
-        pageSize: 50,
-      });
+interface Experience {
+  language: String;
+  yoe: number;
+}
 
-      setProjects(
-        list
-          .map(ProjectModel.from)
-          .filter((p) => p.stargazersCount >= 0 && !p.archived && !p.disabled)
-          .sort((a, b) => b.stargazersCount - a.stargazersCount),
-      );
-    }, []),
-    true,
+const technologies: Experience[] = [
+  {
+    language: 'Docker',
+    yoe: 2,
+  },
+  {
+    language: 'PHP',
+    yoe: 2,
+  },
+  {
+    language: 'MYSQL',
+    yoe: 2,
+  },
+];
+
+const LangExperience = ({ language, yoe }: Experience) => {
+  return (
+    <ExpComp>
+      <h1 className="font-bold text-lg">{language}: </h1>
+      <span className="text-sm opacity-75">
+        {yoe} {yoe > 1 ? 'years' : 'year'}{' '}
+      </span>{' '}
+    </ExpComp>
   );
+};
 
-  useEffect(() => {
-    loadProject();
-  }, []);
+export default function Experiences() {
+  const { t } = useTranslation();
 
   return (
     <Wrapper>
       <Title>{t('experiences.title')}</Title>
-
       <List>
         <div>
-          <h2>Tools</h2>
-          <p>Git</p>
+          <h1 className="font-bold text-2xl mb-3">Technologies</h1>
+          <div className="flex flex-wrap gap-3">
+            {technologies?.map((tech, i) => (
+              <LangExperience {...tech} key={i} />
+            ))}
+          </div>
         </div>
-        <div>  
-          <h1>Technologies</h1>
-        </div>
-        <div> 
-          <h1>Languages</h1>
-        </div>
-        <div>
-          <h1>Frontend Frameworks</h1>
-        </div>  
-        <div>
-          <h1>Backend Frameworks</h1>
-        </div>
-        <div>  
-          <h1>Libraries</h1>
-        </div>
-        <div>    
-          <h1>Databases</h1>
-        </div> 
-        </List>
+      </List>
+
+      {/* <div>
+        <h2>Tools</h2>
+        <p>Git</p>
+      </div>
+
+      <div>
+        <h1>Languages</h1>
+      </div>
+      <div>
+        <h1>Frontend Frameworks</h1>
+      </div>
+      <div>
+        <h1>Backend Frameworks</h1>
+      </div>
+      <div>
+        <h1>Libraries</h1>
+      </div>
+      <div>
+        <h1>Databases</h1>
+      </div> */}
     </Wrapper>
   );
 }
