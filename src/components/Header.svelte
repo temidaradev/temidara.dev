ba4971e1c328ae274b87255a2885b891c6851835
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-03 22:09:26 +0300

    will finish tomorrow

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -23,15 +23,15 @@
         <p>{tab.name}</p>
       </a>
     {/each}
+    <a
+      target="_blank"
+      href="https://x.com/temidaradev"
+      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+    >
+      <div
+        class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+      ></div>
+      <h4 class="relative z-9">Get In Touch</h4>
+    </a>
   </div>
-  <a
-    target="_blank"
-    href="https://x.com/temidaradev"
-    class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-  >
-    <div
-      class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-    ></div>
-    <h4 class="relative z-9">Get In Touch</h4>
-  </a>
 </header>
