f5f06894ef10665baae333d38b48f12954ba6df1
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-04 20:30:22 +0300

    navbar for mobile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,22 +2,20 @@
   export let y;
 
   let tabs = [
-    { name: "Projects", link: "#projects" },
-    { name: "About Me", link: "#about" },
+    { name: 'Projects', link: '#projects' },
+    { name: 'About Me', link: '#about' },
     // {name: 'project', '#projects'},
   ];
 </script>
 
 <header
-  class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid " +
-    (y > 0
-      ? " py-4 bg-slate-950 border-violet-950"
-      : " py-6 bg-transparent border-transparent")}
+  class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' +
+    (y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent')}
 >
   <h1 class="font-medium">
     <b class="font-bold poppins">Temidara</b>
   </h1>
-  <div class="sm:flex items-center gap-4 hidden">
+  <div class="sm:flex flex wrap items-center gap-4">
     {#each tabs as tab, index}
       <a href={tab.link} class="duration-200 hover:text-violet-400">
         <p>{tab.name}</p>
