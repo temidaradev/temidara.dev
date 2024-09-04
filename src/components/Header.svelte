1562fc301b1d02fa938329973ac4443633dc2911
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-04 20:59:26 +0300

    transparent navbar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,10 +8,7 @@
   ];
 </script>
 
-<header
-  class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' +
-    (y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent')}
->
+<header class="sticky z-[10] top-0 duration-200 px-6 py-6 flex items-center justify-between">
   <h1 class="font-medium">
     <b class="font-bold poppins">Temidara</b>
   </h1>
