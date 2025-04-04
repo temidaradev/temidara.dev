40b0758ca8d126ee0ebb575f4b51f96313cac5a8
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-04-04 18:23:59 +0300

    my gameeeee

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-  <div>
+  <div class="flex flex-col items-center">
     <h2 class="font-bold text-xl sm:text-2xl md:text-3xl text-violet-400">
       The Unspooled Self
     </h2>
@@ -10,6 +10,16 @@
       This game has a social message at the end. Demo will be out soon in
       itch.io
     </h3>
-    <img src="src/screenshot.jpeg" />
+  </div>
+  <div class="flex flex-col flex-1 p-3 items-center">
+    <iframe
+      frameborder="0"
+      src="https://itch.io/embed/3446861?bg_color=000000&amp;fg_color=ffffff&amp;link_color=2a48ff&amp;border_color=bebebe"
+      width="552"
+      height="167"
+      ><a href="https://jinchuugames.itch.io/the-unspooled-self-beta"
+        >The Unspooled Self Beta by jinchuugames</a
+      ></iframe
+    >
   </div>
 </main>
