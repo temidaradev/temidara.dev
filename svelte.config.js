bcd7442adf44d7986f2025e6a52cfe773e731460
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-08-22 20:58:41 +0300

    dont use build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html",
-    }),
+    adapter: adapter()
   },
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess()
 };
-
 export default config;
