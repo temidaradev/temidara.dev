3a27e0f6c690534569c063a029c196faf335b13b
temidaradev <temidaradev@proton.me>
2025-10-28 22:36:43 +0300

    fix tech blog errors

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
